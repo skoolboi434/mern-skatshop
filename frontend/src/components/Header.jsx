7ba6f054363e8eb8942a66639289160a753f7eee
Travis Davis <skoolboi434@gmail.com>
2026-07-24 20:35:57 -0400

    Data fetched from backend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
